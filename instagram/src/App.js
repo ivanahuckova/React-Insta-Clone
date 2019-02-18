3e842801c02d1dea476bf8addec6b130333f46f8
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-18 14:20:59 +0100

    feat: Create PostContainer for each instagram post

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
 
-class App extends Component {
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { dummyData };
+	}
+
 	render() {
-		return <div className="App" />;
+		return (
+			<div>
+				{this.state.dummyData.map(post => {
+					console.log(post);
+					return <PostContainer post={post} />;
+				})}
+			</div>
+		);
 	}
 }
-
-export default App;
---
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostContainer(props) {
+	return <div>Hello</div>;
+}
