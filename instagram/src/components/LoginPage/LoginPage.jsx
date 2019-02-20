7dfd516ea8b47fa2dd840770c97ef1031a364154
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-20 16:20:01 +0100

    feat: Disabled submit button until added name and password

--- instagram/src/components/LoginPage/LoginPage.jsx
+++ instagram/src/components/LoginPage/LoginPage.jsx
@@ -12,11 +12,12 @@ export default function LoginPage(props) {
 	};
 	const loginName = props.loginName;
 	const loginPassword = props.loginPassword;
+	const isEnabled = loginName.length > 0 && loginPassword.length > 0;
 	return (
 		<form onSubmit={login}>
 			Name: <input value={loginName} onChange={handleAddNameInput} />
 			Password: <input value={loginPassword} onChange={handleAddPasswordInput} />
-			<button type="button" onClick={login}>
+			<button type="button" disabled={!isEnabled} onClick={login}>
 				Submit
 			</button>
 		</form>
