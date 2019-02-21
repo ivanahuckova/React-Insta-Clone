15edc34b60d0fe8076aecdec4b513b9b9647ed4c
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-21 15:10:19 +0100

    style: Create styled components for actionitemscontainer

--- instagram/src/components/SearchBar/components/ActionItemsContainer.jsx
+++ instagram/src/components/SearchBar/components/ActionItemsContainer.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-//css
-import "../SearchBar.css";
+import styled from "styled-components";
 
 export default function ActionItemsContainer(props) {
 	const logout = () => props.logout();
 	return (
-		<div className="searchbar-right-container">
+		<StyledACtionItemsContainer>
 			<img className="searchbar-icon" src="/assets/compass-icon.svg" alt="instagram icon" />
 			<img className="searchbar-icon" src="/assets/heart-icon.svg" alt="instagram icon" />
 			<img className="searchbar-icon" src="/assets/person-icon.svg" alt="instagram icon" />
 			<button onClick={logout} />
-		</div>
+		</StyledACtionItemsContainer>
 	);
 }
 
 ActionItemsContainer.propTypes = {
 	logout: PropTypes.func
 };
+
+const StyledACtionItemsContainer = styled.div`
+	width: 20%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	img {
+		width: 25%;
+		cursor: pointer;
+	}
+`;
